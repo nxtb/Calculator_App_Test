7ab3f93567dcf373acbc8c05dd5e12bdf184d20a
Ash Brindle <ashb@nextbase.co.uk>
2022-08-04 16:07:19 +0100

    remove dismissable

--- lib/todos_overview/widgets/todo_list_tile.dart
+++ lib/todos_overview/widgets/todo_list_tile.dart
@@ -20,48 +20,37 @@ class TodoListTile extends StatelessWidget {
     final theme = Theme.of(context);
     final captionColor = theme.textTheme.caption?.color;
 
-    return Dismissible(
-      key: Key('todoListTile_dismissible_${todo.id}'),
-      onDismissed: onDismissed,
-      direction: DismissDirection.endToStart,
-      background: Container(
+    return Container(
         alignment: Alignment.centerRight,
-        color: theme.colorScheme.error,
+        color: Colors.grey[500],
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: const Icon(
-          Icons.delete,
-          color: Color(0xAAFFFFFF),
-        ),
-      ),
-      child: ListTile(
-        onTap: onTap,
-        title: Text(
-          todo.title,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: !todo.isCompleted
-              ? null
-              : TextStyle(
-                  color: captionColor,
-                  decoration: TextDecoration.lineThrough,
-                ),
-        ),
-        subtitle: Text(
-          todo.description,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-        leading: Checkbox(
-          shape: const ContinuousRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-          ),
-          value: todo.isCompleted,
-          onChanged: onToggleCompleted == null
-              ? null
-              : (value) => onToggleCompleted!(value!),
-        ),
-        trailing: onTap == null ? null : const Icon(Icons.chevron_right),
-      ),
-    );
+        child: ListTile(
+            onTap: onTap,
+            title: Text(
+              todo.title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: !todo.isCompleted
+                  ? null
+                  : TextStyle(
+                      color: captionColor,
+                      decoration: TextDecoration.lineThrough,
+                    ),
+            ),
+            subtitle: Text(
+              todo.description,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            leading: Checkbox(
+              shape: const ContinuousRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+              ),
+              value: todo.isCompleted,
+              onChanged: onToggleCompleted == null
+                  ? null
+                  : (value) => onToggleCompleted!(value!),
+            ),
+            trailing: onTap == null ? null : const Icon(Icons.chevron_right)));
   }
 }
